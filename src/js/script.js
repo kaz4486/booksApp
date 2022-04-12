e4ee27756f2c97dc94546d7933df21661a72e9ea
Kamil <kaz4486@gmail.com>
2022-04-12 17:39:45 +0200

    Add render and initActions methods

---
+++ src/js/script.js
@@ -0,0 +1,58 @@
+const templatesBooks = Handlebars.compile(
+  document.querySelector('#template-book').innerHTML
+);
+console.log(templatesBooks);
+
+/* find books list container */
+
+const booksListContainer = document.querySelector('.books-list');
+console.log(booksListContainer);
+
+function render() {
+  for (const book in dataSource.books) {
+    //generated HTML based on template
+
+    const generatedHTML = templatesBooks(dataSource.books[book]);
+    console.log(generatedHTML);
+    /* create element DOM using utils.createElementFromHTML*/
+
+    const bookElement = utils.createDOMFromHTML(generatedHTML);
+    console.log(bookElement);
+
+    /* add created element to booksList*/
+
+    booksListContainer.appendChild(bookElement);
+  }
+}
+
+//const bookList = document.querySelector('.book');
+
+function initActions() {
+  const imageElements = document.querySelectorAll('.book__image');
+  console.log(imageElements);
+  const favoriteBooks = [];
+  for (let elem of imageElements) {
+    //const elemId = imageElements[data - id];
+    //console.log(elemId);
+    console.log(imageElements);
+    elem.addEventListener('click', function (event) {
+      event.preventDefault();
+      let elemId = event.target.offsetParent.getAttribute('data-id');
+      if (!favoriteBooks.includes(elemId)) {
+        console.log(favoriteBooks[elemId]);
+        console.log(event);
+        event.target.offsetParent.classList.add('favorite');
+
+        favoriteBooks.push(elemId);
+        console.log(favoriteBooks);
+      } else if (favoriteBooks.includes(elemId)) {
+        event.target.offsetParent.classList.remove('favorite');
+        const indexOf = favoriteBooks.indexOf(event.target.offsetParent);
+        favoriteBooks.splice(indexOf, 1);
+      }
+    });
+  }
+}
+
+render();
+initActions();
