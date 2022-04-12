fc3e16a281a56df24d47f559e98a5f5d4b109b52
Kamil <kaz4486@gmail.com>
2022-04-12 18:38:01 +0200

    Modify initActions

--- src/js/script.js
+++ src/js/script.js
@@ -28,30 +28,36 @@ function render() {
 //const bookList = document.querySelector('.book');
 
 function initActions() {
-  const imageElements = document.querySelectorAll('.book__image');
-  console.log(imageElements);
+  /*const imageElements = document.querySelectorAll('.book__image');
+  console.log(imageElements);*/
   const favoriteBooks = [];
-  for (let elem of imageElements) {
-    //const elemId = imageElements[data - id];
-    //console.log(elemId);
-    console.log(imageElements);
-    elem.addEventListener('click', function (event) {
-      event.preventDefault();
-      let elemId = event.target.offsetParent.getAttribute('data-id');
-      if (!favoriteBooks.includes(elemId)) {
-        console.log(favoriteBooks[elemId]);
-        console.log(event);
-        event.target.offsetParent.classList.add('favorite');
+  //for (let elem of imageElements) {
+  //const elemId = imageElements[data - id];
+  //console.log(elemId);
+  //console.log(imageElements);
+  booksListContainer.addEventListener('click', function (event) {
+    event.preventDefault();
+    let elemId = event.target.offsetParent.getAttribute('data-id');
 
-        favoriteBooks.push(elemId);
-        console.log(favoriteBooks);
-      } else if (favoriteBooks.includes(elemId)) {
-        event.target.offsetParent.classList.remove('favorite');
-        const indexOf = favoriteBooks.indexOf(event.target.offsetParent);
-        favoriteBooks.splice(indexOf, 1);
-      }
-    });
-  }
+    if (
+      !favoriteBooks.includes(elemId) &&
+      event.target.offsetParent.classList.contains('book__image')
+    ) {
+      console.log(favoriteBooks[elemId]);
+      console.log(event);
+      event.target.offsetParent.classList.add('favorite');
+
+      favoriteBooks.push(elemId);
+      console.log(favoriteBooks);
+    } else if (
+      favoriteBooks.includes(elemId) &&
+      event.target.offsetParent.classList.contains('book__image')
+    ) {
+      event.target.offsetParent.classList.remove('favorite');
+      const indexOf = favoriteBooks.indexOf(event.target.offsetParent);
+      favoriteBooks.splice(indexOf, 1);
+    }
+  });
 }
 
 render();
